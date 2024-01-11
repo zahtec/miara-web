c7fb031a6349707bdb7ced01db7b13fdf1438fb0
zahtec <32808999+zahtec@users.noreply.github.com>
2024-01-11 00:04:09 -0800

    fix: externalize argon2

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import Icons from "unplugin-icons/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 
 export default defineConfig({
-	plugins: [sveltekit(), Icons({ compiler: "svelte" })]
+	plugins: [sveltekit(), Icons({ compiler: "svelte" })],
+	ssr: {
+		external: ["argon2"]
+	}
 });
